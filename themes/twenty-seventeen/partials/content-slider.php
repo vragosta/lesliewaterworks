eac57abe78a6dd09e5c9965bec410ba2a491691c
Vincent Ragosta <vragosta@storycorps.org>
2017-02-15 16:50:53 -0500

    pushing placeholder for checkpoint merge to dev

--- themes/twenty-seventeen/partials/content-slider.php
+++ themes/twenty-seventeen/partials/content-slider.php
@@ -17,8 +17,8 @@
 	$query = new WP_Query( $args );
 
 	while ( $query->have_posts() ) : $query->the_post();
-		echo 'ID: ' . $post->ID;
+		// echo 'ID: ' . $post->ID;
 	endwhile;
-	wp_reset_postdata();
+	wp_reset_postdata(); ?>
 
-?>
+	<section style="display: flex; justify-content: center; align-items: center; height: 400px; border: 1px solid #ddd; margin: 2rem;">Placeholder</section>
