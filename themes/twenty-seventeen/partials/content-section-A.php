d299481e7e2a8c7883d90f68adfa88dcf7290c4e
frankieb137 <fbordone@storycorps.net>
2017-02-28 17:25:52 -0500

    Code cleanup; slight tweaks to wp-query; need to still add button to link to archive page

--- themes/twenty-seventeen/partials/content-section-A.php
+++ themes/twenty-seventeen/partials/content-section-A.php
@@ -13,34 +13,34 @@ $args = array(
 $new_watercoolers = new WP_Query( $args ); ?>
 
 <section class="container-fluid">
-  <!-- Section title (TBD) -->
-  <h2>Brand New Products:</h2>
+	<!-- Section title -->
+	<h2>Brand New Products:</h2>
 
-  <div class="row" style="padding: 15px;"><?php
-    if ( $new_watercoolers->have_posts() ) :
-      while ( $new_watercoolers->have_posts() ) : $new_watercoolers->the_post(); ?>
+	<div class="row new-watercoolers"><?php
+		if ( $new_watercoolers->have_posts() ) :
+			while ( $new_watercoolers->have_posts() ) : $new_watercoolers->the_post(); ?>
 
-        <div class="col-md-6">
+				<div class="col-md-12 col-lg-6">
+					<div class="row">
+						<div class="col-md-4">
+							<!-- Post thumbnail with appropriate permalink -->
+							<a href="<?php echo get_the_permalink( $post->ID ); ?>"><?php the_post_thumbnail( array( 400, 400 ) ); ?></a>
+						</div>
+						<div class="col-md-8">
+							<!-- Post title -->
+							<h2><?php the_title(); ?></h2>
 
-          <div class="row">
-            <div class="col-md-4">
-              <a href="<?php echo get_the_permalink( $post->ID ); ?>"><?php the_post_thumbnail( array( 400, 400 ) ); ?></a>
-            </div>
-            <div class="col-md-8">
-              <!-- Post title -->
-              <h2 style="margin-top: 0; margin-bottom: 20px;"><?php the_title(); ?></h2>
+							<!-- Post content -->
+							<?php the_content(); ?>
 
-              <!-- Post content -->
-              <?php the_content(); ?>
+							<!-- 'Learn more' button with appropriate permalink -->
+							<a href="<?php echo get_the_permalink( $post->ID ); ?>"><button class="btn btn-default btn-md">Learn more</button></a>
+						</div>
+					</div>
+				</div><?php
 
-              <a href="<?php echo get_the_permalink( $post->ID ); ?>"><button class="btn btn-default btn-md">Learn more</button></a>
-            </div>
-          </div>
-
-        </div><?php
-
-      endwhile;
-      wp_reset_postdata();
-    endif; ?>
-  </div>
+			endwhile;
+			wp_reset_postdata();
+		endif; ?>
+	</div>
 </section>
--- themes/twenty-seventeen/partials/content-section-B.php
+++ themes/twenty-seventeen/partials/content-section-B.php
@@ -6,29 +6,46 @@
  * @since 0.1.0
  */
 
+// Argument to obtain two most recent watercoolers excluding the new ones.
 $args = array(
-	'post_type'      => array( 'watercooler', 'icemachine' ),
-	'posts_per_page' => 8,
-  'post__not_in' => array( 52, 55 ),
+	'post_type'      => array( 'watercooler' ),
+	'posts_per_page' => 2,
+	'post__not_in' => array( 31, 34 ), // 52, 55 at home
 );
-$misc_products = new WP_Query( $args ); ?>
+
+// Argument to obtain two most recent icecoolers.
+$args2 = array(
+	'post_type'      => array( 'icemachine' ),
+	'posts_per_page' => 2,
+);
+
+$misc_watercoolers = new WP_Query( $args );
+$misc_icemachines  = new WP_Query( $args2 ); ?>
 
 <section class="container-fluid">
-  <h2>Our Other Products:</h2><?php
-    if ( $misc_products->have_posts() ) :
-      while ( $misc_products->have_posts() ) : $misc_products->the_post(); ?>
-        <div class="row" style="padding: 15px; margin-bottom: 20px;">
-          <div class="col-md-3">
-            <a href="<?php echo get_the_permalink( $post->ID ); ?>"><?php the_post_thumbnail( array( 400, 400 ) ); ?></a>
-          </div>
-          <div class="col-md-9">
-            <h2 style="margin-top: 0; margin-bottom: 20px;"><?php the_title(); ?></h2>
-            <?php the_content(); ?>
-            <a href="<?php echo get_the_permalink( $post->ID ); ?>"><button class="btn btn-default btn-md">Learn more</button></a>
-          </div>
-        </div><?php
+		<!-- Section title -->
+	<h2>Our Other Products:</h2><?php
 
-      endwhile;
-      wp_reset_postdata();
-    endif; ?>
+		if ( $misc_watercoolers->have_posts() ) :
+			while ( $misc_watercoolers->have_posts() ) : $misc_watercoolers->the_post(); ?>
+				<div class="row" style="padding: 15px; margin-bottom: 20px;">
+					<div class="col-md-3">
+						<!-- Post thumbnail with appropriate permalink -->
+						<a href="<?php echo get_the_permalink( $post->ID ); ?>"><?php the_post_thumbnail( array( 400, 400 ) ); ?></a>
+					</div>
+					<div class="col-md-9">
+						<!-- Post title -->
+						<h2 style="margin-top: 0; margin-bottom: 20px;"><?php the_title(); ?></h2>
+
+						<!-- Post content -->
+						<?php the_content(); ?>
+
+						<!-- 'Learn more' button with appropriate permalink -->
+						<a href="<?php echo get_the_permalink( $post->ID ); ?>"><button class="btn btn-default btn-md">Learn more</button></a>
+					</div>
+				</div><?php
+
+			endwhile;
+			wp_reset_postdata();
+		endif; ?>
 </section>
